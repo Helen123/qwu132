0dd54ece234600df3c16242196e2452ef6a685d7
qwu132@ucsb.edu <qwu132@ucsb.edu>
2022-11-10 19:59:18 -0800

    update

--- typo/Heap.cpp
+++ typo/Heap.cpp
@@ -60,11 +60,11 @@ using namespace std;
         size_t left=i * 2 + 1;
         size_t right=i * 2 + 2;
         while(left<mCount){
-            if(right==mCount||(mData[left].score<=mData[right].score&&mData[left].score<mData[i].score)){
+            if((right==mCount&&mData[left].score<mData[i].score)||(mData[left].score<=mData[right].score&&mData[left].score<mData[i].score)){
                 swap(mData[left], mData[i]);
                 i=i * 2 + 1;
             }
-            else if(mData[left].score>mData[right].score&&mData[right].score<mData[i].score){
+            else if(mData[right].score<mData[i].score){
                 swap(mData[right], mData[i]);
                  i=i * 2 + 2;
             }
